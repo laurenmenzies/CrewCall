9f0924dc9f66f3ae146aee77916d444db9f667e5
Lauren Menzies <laurenmenzies@gmail.com>
2020-06-26 10:47:27 -0400

    React Native In Progress

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+
+export default Header() {
+    return (
+        <View>
+            <Text>CREW CALL</Text>
+        </View>
+    )
+}
---
+++ components/ProductionPage.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Button, Text, View, TextInput } from 'react-native';
+
+export default function ProductionPage() {
+    return (
+        <View> 
+            <Text>Production Name</Text>
+            <View>
+                Production Members
+            </View>
+                // tasks
+        </View>
+    )
+}
